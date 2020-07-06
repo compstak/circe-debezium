15ff68f4eb6f4842a7ea0b11090e2ddf1a72f801
Luka Jacobowitz <luka.jacobowitz@gmail.com>
2020-07-06 16:26:02 -0400

    Encode schema first

--- src/main/scala/compstak/circe/debezium/DebeziumKey.scala
+++ src/main/scala/compstak/circe/debezium/DebeziumKey.scala
@@ -4,14 +4,14 @@ import io.circe.{Decoder, Encoder, Json}
 import io.circe.syntax._
 import cats.implicits._
 
-case class DebeziumKey[A](payload: DebeziumKeyPayload[A], schema: Json)
+case class DebeziumKey[A](schema: Json, payload: DebeziumKeyPayload[A])
 
 object DebeziumKey {
   implicit def decoder[A: Decoder]: Decoder[DebeziumKey[A]] =
-    Decoder.forProduct2("payload", "schema")(DebeziumKey.apply[A])
+    Decoder.forProduct2("schema", "payload")(DebeziumKey.apply[A])
 
   implicit def encoder[A: Encoder]: Encoder[DebeziumKey[A]] =
-    Encoder.forProduct2("payload", "schema")(k => (k.payload, k.schema))
+    Encoder.forProduct2("schema", "payload")(k => (k.payload, k.schema))
 }
 
 case class DebeziumKeyPayload[A](id: A, idName: String)
