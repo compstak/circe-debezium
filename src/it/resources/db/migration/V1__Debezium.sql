5363754085bd19416da9d7cd06dbb5132bf94675
Paul Snively <psnively@mac.com>
2020-07-31 14:46:13 -0400

    Prepare PostgreSQL schema for circe-debezium testing.

---
+++ src/it/resources/db/migration/V1__Debezium.sql
@@ -0,0 +1,10 @@
+create table DEBEZIUM (
+    FIELD1 integer           not null,
+    FIELD2 smallint          not null,
+    FIELD3 bigint            not null,
+    FIELD4 boolean           not null,
+    FIELD5 text              not null,
+    FIELD6 real              not null,
+    FIELD7 double precision  not null,
+    FIELD8 integer           null
+);
