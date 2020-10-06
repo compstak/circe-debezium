741e8b0465ccbd9a437ab25bdafc8a66b70eff16
Luka Jacobowitz <luka.jacobowitz@gmail.com>
2020-10-06 16:30:29 -0400

    Add DebeziumValueState alias for optional before field

--- src/main/scala/compstak/circe/debezium/package.scala
+++ src/main/scala/compstak/circe/debezium/package.scala
@@ -1,6 +1,9 @@
 package compstak.circe
 
+import io.circe.Json
+
 package object debezium {
   type DebeziumValue[A] = DebeziumValue2[A, A]
+  type DebeziumValueState[A] = DebeziumValue2[Option[Json], A]
   type DebeziumPayload[A] = DebeziumPayload2[A, A]
 }
